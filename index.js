cbac1c6c0fd943c42a0040022bd246ca17bcbeef
JustinMitchell <mitchelljm04@gmail.com>
2022-01-17 22:18:55 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickUpBlock){
+    return Math.abs(42 - pickUpBlock)
+}
+function distanceFromHqInFeet(pickUpBlock){
+    return distanceFromHqInBlocks(pickUpBlock)* 264
+}
+
+function distanceTravelledInFeet(start, distance){
+    return Math.abs((start-distance)*264)
+}
+
+function calculatesFarePrice(start, distance){ 
+    const destination = distanceTravelledInFeet(start,distance)
+    if (destination < 400){
+        return 0;
+    }
+    else if (destination > 400 && destination < 2000) {
+        return (destination - 400) * .02
+    }
+    else if (destination > 2000  && destination < 2500) {
+        return 25
+    }
+    else if (destination > 2500) {
+        return 'cannot travel that far'
+    }
+}
